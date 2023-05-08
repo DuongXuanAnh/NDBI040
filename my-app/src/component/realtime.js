9e98907446fb814c36c9514a5576b096500571cc
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2023-05-08 16:05:58 +0200

    second query init

--- my-app/src/component/realtime.js
+++ my-app/src/component/realtime.js
@@ -1,4 +1,5 @@
-import { onValue, ref, set, push, child, get } from 'firebase/database';
+import { onValue, ref, set, push, child, get, query, orderByChild, equalTo, startAt, endAt, on } from 'firebase/database';
+
 
 import { db } from "../config/firebase";
 
@@ -103,28 +104,47 @@ export const Realtime = () => {
         };
         const orderItem3Ref = push(orderItemsRef);
         set(orderItem3Ref, orderItem3);
-      }
+      };
 
 
 
-    const readRealTimeDB = async () => {
-
-      // Zobrazit tabulku User
-      // const usersRef = ref(db, "users");
-      // onValue(usersRef, (snapshot) => {
-      //   const usersData = snapshot.val();
-      //   const usersList = [];
-
-      //   for (const [key, value] of Object.entries(usersData)) {
-      //     usersList.push({ id: key, ...value });
-      //   }
-      //   console.log(usersList);
-      // });
-      //--------------------------------------------------------------------------------------
-
-     
+      const getUserIdsByCity = async (city) => {
+        const usersRef = ref(db, "users");
+        const usersQuery = query(usersRef, orderByChild("address/city"), equalTo(city));
+        const snapshot = await get(usersQuery);
       
-    }
+        const userIds = [];
+        snapshot.forEach((childSnapshot) => {
+          userIds.push(childSnapshot.key);
+        });
+      
+        return userIds;
+      };
+      
+      const readRealTimeDB = async () => {
+        // Get user IDs of users who live in "Anytown USA"
+        const userIds = await getUserIdsByCity("Anytown USA");
+      
+        // Retrieve all orders placed by users who live in the city "Anytown USA"
+        const ordersRef = ref(db, "orders");
+        const ordersQuery = query(ordersRef, orderByChild("user_id"), startAt(userIds[0]), endAt(userIds[userIds.length - 1]));
+      
+        const ordersSnapshot = await get(ordersQuery);
+        ordersSnapshot.forEach(async (orderSnapshot) => {
+          const orderData = orderSnapshot.val();
+      
+          // Check if the order's user_id is in the list of user IDs
+          if (userIds.includes(orderData.user_id)) {
+            // Get the user's name for this order
+            const userRef = ref(db, `users/${orderData.user_id}`);
+            const userSnapshot = await get(userRef);
+            const userName = userSnapshot.val().name;
+      
+            // Print the order ID, total price, order date, and user name to the console
+            console.log(orderSnapshot.key, "=>", "Total Price:", orderData.total_price, "Order Date:", orderData.order_date, "User Name:", userName);
+          }
+        });
+      };
 
     return (
         <div>
@@ -133,3 +153,4 @@ export const Realtime = () => {
         </div>
     )
 }
+
