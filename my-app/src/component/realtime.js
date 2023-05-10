7bf484f54eafd596383636cd5c072d5666b40a3f
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2023-05-10 14:56:16 +0200

    update second query

--- my-app/src/component/realtime.js
+++ my-app/src/component/realtime.js
@@ -115,71 +115,38 @@ export const Realtime = () => {
 
       // -------------------------------------------------------------------------------------------------
 
-      // Funkce fetchData načte data z databáze Firebase a převede je na pole objektů, 
-      // kde každý objekt reprezentuje jeden záznam v databázi. 
-      // Každý objekt obsahuje id a hodnoty vlastností získané z Firebase pomocí childSnapshot.val().
-
-      
-      // childSnapshot v kódu odkazuje na jedno dítě datového uzlu v Realtime Database. 
-      // Při použití metody snapshot.forEach() v kódu, funkce iteruje přes všechny potomky 
-      // daného uzlu a v každém kroku cyklu se childSnapshot odkazuje na aktuální potomka.
-      // childSnapshot je typicky používán pro získání dat potomka z Realtime Database pomocí metody childSnapshot.val(),
-      //  která vrací hodnoty potomka jako objekt.
-      
-      const fetchData = async (nodeRef) => {
-        const snapshot = await get(nodeRef);
-        const data = [];
-        snapshot.forEach((childSnapshot) => {
-          data.push({ id: childSnapshot.key, ...childSnapshot.val() });
-        });
-        return data;
-      };
-
-
-      // Funkce calculateRevenuePerUser spočítá celkové příjmy za každého uživatele v databázi
-      // na základě dat získaných pomocí fetchData. Iteruje přes všechny uživatele a pro každého z nich
-      // projde všechny objednávky a záznamy v objednávkách a spočítá celkové příjmy pro každého uživatele. 
-      // Výsledkem je objekt revenuePerUser, který obsahuje jméno, email a celkové příjmy za každého uživatele.
-      const calculateRevenuePerUser = (users, orders, orderItems) => {
-        const revenuePerUser = {};
-      
-        for (const user of users) {
-          let totalRevenue = 0;
-      
-          for (const order of orders) {
-            if (order.user_id === user.id) {
-              for (const orderItem of orderItems) {
-                if (orderItem.order_id === order.id) {
-                  totalRevenue += order.total_price * orderItem.quantity;
-                }
-              }
-            }
-          }
-      
-          revenuePerUser[user.id] = {
-            name: user.name,
-            email: user.email,
-            revenue: totalRevenue,
-          };
-        }
-      
-        return revenuePerUser;
-      };
-
-      // Funkce readRealTimeDB získá referenci na datové uzly v Realtime Database, 
-      // načte data pomocí fetchData a vypočítá celkové příjmy za každého uživatele pomocí calculateRevenuePerUser. 
-      // Výsledky jsou vypsány na konzoli pomocí console.log().
       const readRealTimeDB = async () => {
-        const usersRef = ref(db, "users");
+        // Vytvoření reference na uzel "orders" v databázi
         const ordersRef = ref(db, "orders");
-        const orderItemsRef = ref(db, "orderItems");
-
-        const usersData = await fetchData(usersRef);
-        const ordersData = await fetchData(ordersRef);
-        const orderItemsData = await fetchData(orderItemsRef);
-
-        const revenuePerUser = calculateRevenuePerUser(usersData, ordersData, orderItemsData);
-        console.log("Revenue per user:", revenuePerUser);
+        // Vytvoření dotazu, který získá všechny dětské uzly uzlu "orders"
+        const ordersQuery = query(ordersRef);
+        // Získání snapshotu výsledků dotazu pomocí funkce "get()"
+        const ordersSnapshot = await get(ordersQuery);
+      
+        // Iterace přes každý dětský uzel uzlu "orders"
+        for (const orderId in ordersSnapshot.val()) {
+          // Extrahování dat objednávky ze snapshotu
+          const order = ordersSnapshot.val()[orderId];
+          // Extrahování ID uživatele z dat objednávky
+          const userId = order.user_id;
+          // Vytvoření reference na odpovídající uzel uživatele v databázi
+          const userRef = ref(db, "users/" + userId);
+          // Získání snapshotu uzlu uživatele pomocí funkce "get()"
+          const userSnapshot = await get(userRef);
+          // Extrahování jména uživatele ze snapshotu
+          const userName = userSnapshot.val().name;
+          // Vypsání dat objednávky a jména uživatele do konzole pomocí funkce "console.log()"
+          console.log(
+            orderId,
+            "=>",
+            "Celková cena:",
+            order.total_price,
+            "Datum objednávky:",
+            order.order_date,
+            "Jméno uživatele:",
+            userName
+          );
+        }
       };
 
     return (
